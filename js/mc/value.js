4cd804fe970d1c03e2f7f46d799efd99592ea099
Philip Horger <philip@inspire.com>
2014-04-18 09:36:13 -0700

    Add mcValue class

---
+++ js/mc/value.js
@@ -0,0 +1,52 @@
+define('js/mc/value', [], function() {
+
+function mcValue() {
+    // Make this data available to methods, as closures,
+    // But do not leak it to the outside world.
+    var _private = {
+        'value': undefined,
+        'set': false,
+        'subscriptions': []
+    };
+
+    // On change, do callbacks
+    function onChange() {
+        for (var i = 0; i < _private.subscriptions.length; i++) {
+            var callback = _private.subscriptions[i];
+            if (typeof callback == 'function') {
+                callback(this)
+            }
+        }
+    }
+
+    this.getValue = function() {
+        return _private.value;
+    }
+    this.setValue = function(new_value) {
+        _private.value = new_value;
+        _private.set = true;
+        onChange();
+    }
+
+    this.isSet = function() {
+        return _private.set;
+    }
+    this.unSet = function() {
+        _private.value = undefined;
+        _private.set = false;
+        onChange();
+    };
+
+    // Provide ID of registered callback
+    this.subscribe = function(callback) {
+        var length = _private.subscriptions.push(callback);
+        return length-1;
+    }
+    this.unSubscribe = function(id) {
+        _private.subscriptions[id] = null;
+    }
+};
+
+return mcValue;
+
+});
