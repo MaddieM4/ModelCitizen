787238bd86eda16853023b29eeb47a38c7e92c1f
Philip Horger <philip@inspire.com>
2014-04-23 15:03:01 -0700

    Lay out initial API routes

--- srv/api.js
+++ srv/api.js
@@ -1,9 +1,17 @@
 define('api', ['express', 'db'], function(express, db) {
     var app = express();
 
+    // Routes
     app.get('/', function(req, res) {
         res.json({success:true});
     });
+    app.route('/resp/:survey/:response/')
+        .get(function(req, res) {
+            res.json(req.params)
+        })
+        .post(function(req, res) {
+            res.json(req.params)
+        })
 
     // Handle anything that doesn't hit a route
     app.use(function(req, res) {
