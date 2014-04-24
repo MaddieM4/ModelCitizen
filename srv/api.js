53d2143b97ab6f3506e4a4d886474daa20b72373
Philip Horger <philip@inspire.com>
2014-04-24 08:20:55 -0700

    Full support for current API spec

--- srv/api.js
+++ srv/api.js
@@ -1,5 +1,6 @@
-define('api', ['express', 'db'], function(express, db) {
+define('api', ['express','body-parser','db'], function(express, bp, db) {
     var app = express();
+    app.use(bp());
 
     // Routes
     app.get('/', function(req, res) {
@@ -7,10 +8,45 @@ define('api', ['express', 'db'], function(express, db) {
     });
     app.route('/resp/:survey/:response/')
         .get(function(req, res) {
-            res.json(req.params)
+            var sql = 'SELECT value FROM response WHERE '
+                +'survey_name = $survey '
+                +'AND response_name = $response;';
+            var params = {
+                $survey: req.params.survey,
+                $response: req.params.response
+            };
+            db.get(sql, params, function(err, row) {
+                if (err !== null) {
+                    console.error(err);
+                    res.json(500, { error: err });
+                }
+                if (row === undefined) {
+                    // No data, but not an error. Client side will legitimately
+                    // interpret both err and value to be undefined.
+                    res.json({});
+                } else {
+                    // Requires a bit of unpacking
+                    res.json({value: row.value});
+                }
+            });
         })
         .post(function(req, res) {
-            res.json(req.params)
+            var sql = 'INSERT OR REPLACE INTO response '
+                +'(survey_name, response_name, value) '
+                +'VALUES($survey, $response, $value)';
+            var params = {
+                $survey: req.params.survey,
+                $response: req.params.response,
+                $value: req.body.value
+            };
+            db.run(sql, params, function(err) {
+                if (err === null) {
+                    res.json({success: true, value: params.$value})
+                } else {
+                    console.error(err)
+                    res.json({success: false, error: err})
+                }
+            });
         })
 
     // Handle anything that doesn't hit a route
--- srv/db.js
+++ srv/db.js
@@ -1,2 +1,22 @@
 define('db', ['fs', 'sqlite3'], function(fs, sqlite3) {
+    var filename = 'db.sqlite';
+    exists = fs.existsSync(filename);
+
+    sqlite3.verbose();
+    var db = new sqlite3.Database(filename);
+
+    db.serialize(function(){
+        if (!exists) {
+            db.run('CREATE TABLE surveys (name VARCHAR(255) PRIMARY KEY)');
+            db.run('CREATE TABLE response ('
+                +'survey_name VARCHAR(255) REFERENCES surveys(name),'
+                +'response_name VARCHAR(255),'
+                +'value TEXT)');
+            db.run('CREATE UNIQUE INDEX response_location '
+                +'ON response(survey_name, response_name)');
+            db.run('CREATE TABLE migrations (date VARCHAR(128), version INTEGER)');
+        }
+    });
+
+    return db;
 });
